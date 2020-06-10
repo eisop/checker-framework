7c813e64e6d78cf56feaa9bee930afadc04b9c44
xingweitian <xingweitian@gmail.com>
2020-06-09 22:11:34 -0400

    Fix error prone warning on AbstractViewpointAdapter.

--- framework/src/main/java/org/checkerframework/framework/type/AbstractViewpointAdapter.java
+++ framework/src/main/java/org/checkerframework/framework/type/AbstractViewpointAdapter.java
@@ -37,6 +37,7 @@ import javax.lang.model.type.TypeKind;
  * <p>Subclasses implement the computation of the precise viewpoint adapted type given a receiver
  * type and a declared type, and implement how to extract the qualifier given an ATM.
  */
+@SuppressWarnings("PublicConstructorForAbstractClass")
 public abstract class AbstractViewpointAdapter implements ViewpointAdapter {
 
     // This prevents calling combineTypeWithType on type variable if it is an upper bound
