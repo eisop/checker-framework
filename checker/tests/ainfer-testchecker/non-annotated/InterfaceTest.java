a064c97aa18f7a8a81d22b875931443a1d4ac6e5
Michael Ernst <mernst@cs.washington.edu>
2021-07-19 11:56:10 -0700

    Check enum fields just like class fields

--- checker/tests/ainfer-testchecker/non-annotated/InterfaceTest.java
+++ checker/tests/ainfer-testchecker/non-annotated/InterfaceTest.java
@@ -13,7 +13,6 @@ interface InterfaceTest {
   default void requireSibling1(@Sibling1 String x) {}
 
   default void testX() {
-    // :: warning: argument.type.incompatible
     requireSibling1(toaster);
   }
 }
