0854d6a23217bb98eaefe1cc915ca9f4f2b3ec97
Michael Ernst <mernst@cs.washington.edu>
2021-07-31 13:37:59 -0700

    Documentation tweaks
    Co-authored-by: Neil Brown <neil@twistedsquare.com>

--- framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
+++ framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
@@ -1079,7 +1079,7 @@ public class AnnotationFileParser {
             decl.getAnnotations(),
             decl);
       } catch (ErrorTypeKindException e) {
-        // Do nothing, per Issue #244.
+        // Do nothing, per https://github.com/typetools/checker-framework/issues/244 .
       }
     } else {
       assert decl.isConstructorDeclaration();
