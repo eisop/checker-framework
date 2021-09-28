72bedc057af194892db7f36ddbd9cc06562edabc
d367wang <d367wang@uwaterloo.ca>
2021-09-28 15:47:59 -0400

    add updated files

--- framework/src/main/java/org/checkerframework/framework/flow/CFStore.java
+++ framework/src/main/java/org/checkerframework/framework/flow/CFStore.java
@@ -3,8 +3,28 @@ package org.checkerframework.framework.flow;
 /** The default store used in the Checker Framework. */
 public class CFStore extends CFAbstractStore<CFValue, CFStore> {
 
+    /**
+     * Constructor to create a non-bottom CFStore.
+     *
+     * @param analysis the analysis class this store belongs to
+     * @param sequentialSemantics should the analysis use sequential Java semantics?
+     */
     public CFStore(CFAbstractAnalysis<CFValue, CFStore, ?> analysis, boolean sequentialSemantics) {
-        super(analysis, sequentialSemantics);
+        this(analysis, sequentialSemantics, false);
+    }
+
+    /**
+     * Constructor for CFStore.
+     *
+     * @param analysis the analysis class this store belongs to
+     * @param sequentialSemantics should the analysis use sequential Java semantics?
+     * @param isBottom is the store a bottom store?
+     */
+    public CFStore(
+            CFAbstractAnalysis<CFValue, CFStore, ?> analysis,
+            boolean sequentialSemantics,
+            boolean isBottom) {
+        super(analysis, sequentialSemantics, isBottom);
     }
 
     /**
