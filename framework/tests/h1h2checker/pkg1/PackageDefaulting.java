7895a0d3a6a44c145e65365434a18d92b15e39d7
Werner Dietl <wdietl@gmail.com>
2022-01-07 08:44:57 -0500

    Add test that applies defaults to subpackages

--- framework/tests/h1h2checker/pkg1/PackageDefaulting.java
+++ framework/tests/h1h2checker/pkg1/PackageDefaulting.java
@@ -2,10 +2,20 @@ package pkg1;
 
 import org.checkerframework.framework.testchecker.h1h2checker.quals.H1S1;
 import org.checkerframework.framework.testchecker.h1h2checker.quals.H1S2;
+import org.checkerframework.framework.testchecker.h1h2checker.quals.H2S1;
+import org.checkerframework.framework.testchecker.h1h2checker.quals.H2S2;
 
-/* Default from package pkg1 applies within the package. */
+/* Defaults from package pkg1 apply within the package. */
 public class PackageDefaulting {
-    void m(@H1S1 Object p1, @H1S2 Object p2) {
+    // Test H1 hierarchy
+    void m(@H1S1 @H2S1 Object p1, @H1S2 @H2S1 Object p2) {
+        Object l1 = p1;
+        // :: error: (assignment.type.incompatible)
+        Object l2 = p2;
+    }
+
+    // Test H2 hierarchy
+    void m2(@H1S1 @H2S1 Object p1, @H1S1 @H2S2 Object p2) {
         Object l1 = p1;
         // :: error: (assignment.type.incompatible)
         Object l2 = p2;
