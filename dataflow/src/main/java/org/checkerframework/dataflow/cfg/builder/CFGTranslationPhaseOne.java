7da1c962dbc98101a7c3b3d7aea8263ca00263d7
Suzanne Millstein <smillst@cs.washington.edu>
2022-01-28 17:50:43 -0800

    Type-check and refine binding patterns

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGTranslationPhaseOne.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGTranslationPhaseOne.java
@@ -520,8 +520,8 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
       // Must use String comparison to support compiling on JDK 11 and earlier.
       //     Features added between JDK 12 and JDK 17 inclusive.
       switch (tree.getKind().name()) {
-          // case "BINDING_PATTERN":
-          //  return visitBindingPattern17(path.getLeaf(), p);
+        case "BINDING_PATTERN":
+          return visitBindingPattern17(path.getLeaf(), p);
         case "SWITCH_EXPRESSION":
           return visitSwitchExpression17(tree, p);
         case "YIELD":
@@ -560,6 +560,23 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
     Node res = switchBuilder.build();
     switchBuilder = oldSwitchBuilder;
     return res;
+  }
+
+  /**
+   * Visit a BindingPatternTree
+   *
+   * @param bindingPatternTree a BindingPatternTree, typed as Tree to be backward-compatible
+   * @param p parameter
+   * @return the result of visiting the binding pattern tree
+   */
+  public Node visitBindingPattern17(Tree bindingPatternTree, Void p) {
+    ClassTree enclosingClass = TreePathUtil.enclosingClass(getCurrentPath());
+    TypeElement classElem = TreeUtils.elementFromDeclaration(enclosingClass);
+    Node receiver = new ImplicitThisNode(classElem.asType());
+    VariableTree varTree = TreeUtils.bindingPatternTreeGetVariable(bindingPatternTree);
+    LocalVariableNode varNode = new LocalVariableNode(varTree, receiver);
+    extendWithNode(varNode);
+    return varNode;
   }
 
   /* --------------------------------------------------------- */
@@ -3654,7 +3671,11 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
   public Node visitInstanceOf(InstanceOfTree tree, Void p) {
     Node operand = scan(tree.getExpression(), p);
     TypeMirror refType = TreeUtils.typeOf(tree.getType());
-    InstanceOfNode node = new InstanceOfNode(tree, operand, refType, types);
+    Tree binding = TreeUtils.instanceOfGetPattern(tree);
+    LocalVariableNode bindingNode =
+        (LocalVariableNode) ((binding == null) ? null : scan(binding, p));
+
+    InstanceOfNode node = new InstanceOfNode(tree, operand, bindingNode, refType, types);
     extendWithNode(node);
     return node;
   }
