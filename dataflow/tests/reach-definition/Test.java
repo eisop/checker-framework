df69b8e352802dca931b08e169bdfc02c6d32658
Alex Liu <alexliusch@gmail.com>
2022-02-23 10:11:12 -0500

    add reach definition in dfa wild vaild contributor

--- dataflow/tests/reach-definition/Test.java
+++ dataflow/tests/reach-definition/Test.java
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+// This file may not be renamed; it has to have the same filename as ../issue3447/Test.java .
+>>>>>>> 042dca9e0 (add reach definition in dfa wild vaild contributor)
 public class Test {
     public int test() {
         int a = 1, b = 2, c = 3;
