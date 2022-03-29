6ece7c4776abb87b440b48eea25ed8aaebfe39fe
Alex Liu <alexliusch@gmail.com>
2022-03-28 22:16:02 -0400

    reword documentation and add playground

---
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/playground/ReachingDefinitionsPlayground.java
@@ -0,0 +1,34 @@
+package org.checkerframework.dataflow.cfg.playground;
+
+import org.checkerframework.dataflow.analysis.ForwardAnalysis;
+import org.checkerframework.dataflow.analysis.ForwardAnalysisImpl;
+import org.checkerframework.dataflow.cfg.visualize.CFGVisualizeLauncher;
+import org.checkerframework.dataflow.reachingdefinitions.ReachingDefinitionsStore;
+import org.checkerframework.dataflow.reachingdefinitions.ReachingDefinitionsTransfer;
+import org.checkerframework.dataflow.reachingdefinitions.ReachingDefinitionsValue;
+
+public class ReachingDefinitionsPlayground {
+    /**
+     * Run reaching definitions analysis for a specific file and create a PDF of the CFG in the end.
+     *
+     * @param args input arguments, not used
+     */
+    public static void main(String[] args) {
+
+        /* Configuration: change as appropriate */
+        String inputFile = "Test.java"; // input file name and path
+        String outputDir = "cfg"; // output directory
+        String method = "test"; // name of the method to analyze
+        String clazz = "Test"; // name of the class to consider
+
+        // Run the analysis and create a PDF file
+        ReachingDefinitionsTransfer transfer = new ReachingDefinitionsTransfer();
+        ForwardAnalysis<
+                        ReachingDefinitionsValue,
+                        ReachingDefinitionsStore,
+                        ReachingDefinitionsTransfer>
+                forwardAnalysis = new ForwardAnalysisImpl<>(transfer);
+        CFGVisualizeLauncher.generateDOTofCFG(
+                inputFile, outputDir, method, clazz, true, true, forwardAnalysis);
+    }
+}
