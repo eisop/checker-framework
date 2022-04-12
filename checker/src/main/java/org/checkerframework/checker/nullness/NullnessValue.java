d5e5cd755e287c37a642b87553a23749e329fd7f
Haifeng Shi <h223shi@uwaterloo.ca>
2022-04-12 11:29:00 -0400

    reformat

--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessValue.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessValue.java
@@ -89,7 +89,7 @@ public class NullnessValue extends CFAbstractValue<NullnessValue> {
                 + ", "
                 + TypesUtils.simpleTypeName(underlyingType)
                 + ", "
-                + ("poly nn/n= " + (isPolyNullNonNull ? 't' : 'f'))
+                + ("poly nn/n = " + (isPolyNullNonNull ? 't' : 'f'))
                 + ("/" + (isPolyNullNull ? 't' : 'f'))
                 + '}';
     }
