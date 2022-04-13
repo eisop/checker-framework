421c80ed661f9c35357c7b5efd3aeea8ffd999ee
Haifeng Shi <44760135+AndrewShf@users.noreply.github.com>
2022-04-12 21:33:18 -0400

    improve code readability inNullnessValue.java
    Co-authored-by: Werner Dietl <wdietl@gmail.com>

--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessValue.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessValue.java
@@ -89,8 +89,10 @@ public class NullnessValue extends CFAbstractValue<NullnessValue> {
                 + ", "
                 + TypesUtils.simpleTypeName(underlyingType)
                 + ", "
-                + ("poly nn/n = " + (isPolyNullNonNull ? 't' : 'f'))
-                + ("/" + (isPolyNullNull ? 't' : 'f'))
+                + "poly nn/n = "
+                + (isPolyNullNonNull ? 't' : 'f')
+                + "/"
+                + (isPolyNullNull ? 't' : 'f')
                 + '}';
     }
 }
