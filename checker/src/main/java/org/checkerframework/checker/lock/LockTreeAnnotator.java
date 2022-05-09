298c25f9094eec8153fde2248abad043dc7efffb
zcai <z44cai@uwaterloo.ca>
2022-05-09 13:03:01 -0400

    remove redundant logic from LockTreeAnnotator

--- checker/src/main/java/org/checkerframework/checker/lock/LockTreeAnnotator.java
+++ checker/src/main/java/org/checkerframework/checker/lock/LockTreeAnnotator.java
@@ -1,50 +1,26 @@
 package org.checkerframework.checker.lock;
 
-import com.sun.source.tree.BinaryTree;
-import com.sun.source.tree.CompoundAssignmentTree;
 import com.sun.source.tree.NewArrayTree;
 
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
-import org.checkerframework.javacutil.TreeUtils;
-import org.checkerframework.javacutil.TypesUtils;
 
+// Note:
+// For any binary operation whose LHS or RHS can be a non-boolean type, and whose resulting
+// type is necessarily boolean, the resulting annotation on the boolean type must be
+// @GuardedBy({}).
+
+// There is no need to enforce that the annotation on the result of &&, ||, etc.  is
+// @GuardedBy({}) since for such operators, both operands are of type @GuardedBy({}) boolean
+// to begin with.
+
+// A boolean or String is always @GuardedBy({}). LockVisitor determines whether
+// the LHS and RHS of this operation can be legally dereferenced.
 public class LockTreeAnnotator extends TreeAnnotator {
 
     public LockTreeAnnotator(AnnotatedTypeFactory atypeFactory) {
         super(atypeFactory);
-    }
-
-    @Override
-    public Void visitBinary(BinaryTree node, AnnotatedTypeMirror type) {
-        // For any binary operation whose LHS or RHS can be a non-boolean type, and whose resulting
-        // type is necessarily boolean, the resulting annotation on the boolean type must be
-        // @GuardedBy({}).
-
-        // There is no need to enforce that the annotation on the result of &&, ||, etc.  is
-        // @GuardedBy({}) since for such operators, both operands are of type @GuardedBy({}) boolean
-        // to begin with.
-
-        if (TreeUtils.isBinaryComparisonOrInstanceOfOperator(node)
-                || TypesUtils.isString(type.getUnderlyingType())) {
-            // A boolean or String is always @GuardedBy({}). LockVisitor determines whether
-            // the LHS and RHS of this operation can be legally dereferenced.
-            type.replaceAnnotation(((LockAnnotatedTypeFactory) atypeFactory).GUARDEDBY);
-
-            return null;
-        }
-
-        return super.visitBinary(node, type);
-    }
-
-    @Override
-    public Void visitCompoundAssignment(CompoundAssignmentTree node, AnnotatedTypeMirror type) {
-        if (TypesUtils.isString(type.getUnderlyingType())) {
-            type.replaceAnnotation(((LockAnnotatedTypeFactory) atypeFactory).GUARDEDBY);
-        }
-
-        return super.visitCompoundAssignment(node, type);
     }
 
     @Override
