e7a720ce461aa2a253b1fcd9c5724a5d8312046b
Michael Ernst <mernst@cs.washington.edu>
2022-07-25 12:05:31 -0700

    Use try-with-resources (#5222)

--- framework/src/main/java/org/checkerframework/framework/util/CheckerMain.java
+++ framework/src/main/java/org/checkerframework/framework/util/CheckerMain.java
@@ -577,11 +577,10 @@ public class CheckerMain {
     if (outputFilename != null) {
       String errorMessage = null;
 
-      try {
-        PrintWriter writer =
-            (outputFilename.equals("-")
-                ? new PrintWriter(System.out)
-                : new PrintWriter(outputFilename, "UTF-8"));
+      try (PrintWriter writer =
+          (outputFilename.equals("-")
+              ? new PrintWriter(System.out)
+              : new PrintWriter(outputFilename, "UTF-8"))) {
         for (int i = 0; i < args.size(); i++) {
           String arg = args.get(i);
 
@@ -595,19 +594,18 @@ public class CheckerMain {
             // Read argfile and include its parameters in the output file.
             String inputFilename = arg.substring(1);
 
-            BufferedReader br = new BufferedReader(new FileReader(inputFilename));
-            String line;
-            while ((line = br.readLine()) != null) {
-              writer.print(line);
-              writer.print(" ");
+            try (BufferedReader br = new BufferedReader(new FileReader(inputFilename))) {
+              String line;
+              while ((line = br.readLine()) != null) {
+                writer.print(line);
+                writer.print(" ");
+              }
             }
-            br.close();
           } else {
             writer.print(arg);
             writer.print(" ");
           }
         }
-        writer.close();
       } catch (IOException e) {
         errorMessage = e.toString();
       }
@@ -831,8 +829,7 @@ public class CheckerMain {
    */
   private List<@FullyQualifiedName String> getAllCheckerClassNames() {
     ArrayList<@FullyQualifiedName String> checkerClassNames = new ArrayList<>();
-    try {
-      final JarInputStream checkerJarIs = new JarInputStream(new FileInputStream(checkerJar));
+    try (JarInputStream checkerJarIs = new JarInputStream(new FileInputStream(checkerJar))) {
       ZipEntry entry;
       while ((entry = checkerJarIs.getNextEntry()) != null) {
         final String name = entry.getName();
@@ -848,7 +845,6 @@ public class CheckerMain {
           checkerClassNames.add(fqName);
         }
       }
-      checkerJarIs.close();
     } catch (IOException e) {
       // Issue a warning instead of aborting execution.
       System.err.printf(
