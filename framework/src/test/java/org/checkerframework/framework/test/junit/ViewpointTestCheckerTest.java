c69b19b6ed553e31cebb30ad1452ab240ef72bed
Werner Dietl <wdietl@gmail.com>
2022-08-04 15:49:28 -0400

    Adapt to new formatting

--- framework/src/test/java/org/checkerframework/framework/test/junit/ViewpointTestCheckerTest.java
+++ framework/src/test/java/org/checkerframework/framework/test/junit/ViewpointTestCheckerTest.java
@@ -7,7 +7,9 @@ import org.junit.runners.Parameterized;
 
 public class ViewpointTestCheckerTest extends CheckerFrameworkPerDirectoryTest {
 
-  /** @param testFiles the files containing test code, which will be type-checked */
+  /**
+   * @param testFiles the files containing test code, which will be type-checked
+   */
   public ViewpointTestCheckerTest(List<File> testFiles) {
     super(testFiles, viewpointtest.ViewpointTestChecker.class, "viewpointtest");
   }
