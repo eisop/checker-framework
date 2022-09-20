9b831340b9278d3f22ae268a18ee151447d8982b
Alex Liu <a299liu@uwaterloo.ca>
2022-09-20 15:02:43 -0400

    refine test case

--- framework/tests/h1h2checker/Issue282.java
+++ framework/tests/h1h2checker/Issue282.java
@@ -1,19 +1,24 @@
 import org.checkerframework.framework.testchecker.h1h2checker.quals.*;
 
-@SuppressWarnings({"super.invocation.invalid", "inconsistent.constructor.type"})
+@SuppressWarnings({"super.invocation.invalid", "inconsistent.constructor.type", "receiver.invalid"})
 public class Issue282 {
     @H1S1 Issue282() {}
 
+    // TODO static
     public class Inner {
         Inner(@H2S2 Issue282 Issue282.this) {}
     }
-
+    // }
+    // @SuppressWarnings({"super.invocation.invalid", "inconsistent.constructor.type",
+    // "receiver.invalid"})
+    // class Foo {
+    //
     public void test1() {
         // The enclosing type is @H1S1 @H2Top, the receiver type is @H1Top @H2Top
         // should it be from its super constructor or its instantiation?
         // :: error: (receiver.invalid)
-        new Issue282().new Inner() {};
+        //        new Issue282().new Inner() {};
         // The enclosing type is @H1Top @H2Top, the receiver type is @H1Top @H2S2
-        //      new Inner();
+        new Inner();
     }
 }
