a859f6e5e76141b968efe0b3563baed57a7be1f4
Haifeng Shi <shihaifeng1998@gmail.com>
2022-10-11 11:25:56 -0400

    improve the document in DefaultQualifierInHierarchy.java

--- checker-qual/src/main/java/org/checkerframework/framework/qual/DefaultQualifierInHierarchy.java
+++ checker-qual/src/main/java/org/checkerframework/framework/qual/DefaultQualifierInHierarchy.java
@@ -13,7 +13,8 @@ import java.lang.annotation.Target;
  *
  * <p>Other defaults can be specified for a checker via the {@link DefaultFor} meta-annotation,
  * which takes precedence over {@code DefaultQualifierInHierarchy}, or via {@code
- * GenericAnnotatedTypeFactory.addCheckedCodeDefaults()}.
+ * GenericAnnotatedTypeFactory.addCheckedCodeDefaults()}. For more details, see <a
+ * href="https://checkerframework.org/manual/#defaults">Default qualifier for the unannotated</a>
  *
  * <p>The {@link DefaultQualifier} annotation, which targets Java code elements, takes precedence
  * over {@code DefaultQualifierInHierarchy}.
