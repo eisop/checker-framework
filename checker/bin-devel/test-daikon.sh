efdf0eb73b865cd2d46e33657029f81619c4ef21
Alex Liu <a299liu@uwaterloo.ca>
2022-10-20 15:39:11 -0400

    refine code

--- checker/bin-devel/test-daikon.sh
+++ checker/bin-devel/test-daikon.sh
@@ -14,11 +14,13 @@ source "$SCRIPTDIR"/build.sh
 # daikon-typecheck: 15 minutes
 "$SCRIPTDIR/.plume-scripts/git-clone-related" codespecs daikon
 cd ../daikon
+# Add flag to nullness checker
+sed -i "s/JAVAC_NULLNESS_ARGS = -processor org.checkerframework.checker.nullness.NullnessChecker -implicit:class -Xlint:-processing -AskipDefs='${ALLSYSTEMS_SKIPDEFS}/JAVAC_NULLNESS_ARGS = -processor org.checkerframework.checker.nullness.NullnessChecker -implicit:class -Xlint:-processing -AskipDefs='${ALLSYSTEMS_SKIPDEFS}' -AconservativeReceiverInitialization=true/g" java/Makefile
 git log | head -n 5
 make compile
 if [ "$TRAVIS" = "true" ] ; then
   # Travis kills a job if it runs 10 minutes without output
-  time make JAVACHECK_EXTRA_ARGS='-Afilenames -AconservativeReceiverInitialization=true' -C java typecheck
+  time make JAVACHECK_EXTRA_ARGS='-Afilenames' -C java typecheck
 else
-  time make JAVACHECK_EXTRA_ARGS='-AconservativeReceiverInitialization=true' -C java typecheck
+  time make -C java typecheck
 fi
