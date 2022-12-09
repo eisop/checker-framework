9e6b758c982ccfae45dbcea4d1078c9880f80238
Alex Liu <a299liu@uwaterloo.ca>
2022-12-09 17:19:41 -0500

    final version v1

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/ObjectCreationNode.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/ObjectCreationNode.java
@@ -31,6 +31,7 @@ public class ObjectCreationNode extends Node {
 
     // TODO: See issue 376
     /** The constructor node of the object creation. */
+    // TODO: Change its name to identifier in a separate pr
     protected final Node constructor;
 
     /** The arguments of the object creation. */
