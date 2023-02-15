0687d650c12498d475ffd64a7eaff77d414d7ed7
Werner Dietl <wdietl@gmail.com>
2023-02-15 18:36:52 -0500

    Move JSpecify build to separate tests

---
+++ checker/bin-devel/test-jspecify.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+echo "SHELLOPTS=${SHELLOPTS}"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
+
+
+if type -p java; then
+  _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+  _java="$JAVA_HOME/bin/java"
+else
+  echo "Can't find java"
+  exit 1
+fi
+version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+
+if [[ "$version" -ge 9 ]]; then
+  source "$SCRIPTDIR"/build.sh
+
+  ## Build JSpecify and use its tests.
+  "$SCRIPTDIR/.plume-scripts/git-clone-related" jspecify jspecify
+
+  echo "Running:  (cd ../jspecify/ && ./gradlew build)"
+  # If failure, retry in case the failure was due to network lossage.
+  (cd ../jspecify/ && export JDK_JAVA_OPTIONS='--add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED' && (./gradlew build || (sleep 60 && ./gradlew build)))
+  echo "... done: (cd ../jspecify/ && ./gradlew build)"
+
+  # TODO: run tests
+fi
+
