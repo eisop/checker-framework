fb1423d6a121179a4eaba25ef46080ecd64876d9
Alex Liu <a299liu@uwaterloo.ca>
2023-02-20 19:08:53 -0500

    Update framework/src/main/java/org/checkerframework/common/aliasing/AliasingTransfer.java
    Co-authored-by: Werner Dietl <wdietl@gmail.com>

--- framework/src/main/java/org/checkerframework/common/aliasing/AliasingTransfer.java
+++ framework/src/main/java/org/checkerframework/common/aliasing/AliasingTransfer.java
@@ -92,9 +92,7 @@ public class AliasingTransfer extends CFTransfer {
     @Override
     protected void processPostconditions(
             Node n, CFStore store, ExecutableElement executableElement, ExpressionTree tree) {
-        // TODO: Process ObjectCreationNode here after finishing the pull request and addressing the
-        // issue:
-        // https://github.com/eisop/checker-framework/pull/405
+        // TODO: Process ObjectCreationNode here after fixing issue:
         // https://github.com/eisop/checker-framework/issues/400
         if (!(n instanceof MethodInvocationNode)) {
             return;
