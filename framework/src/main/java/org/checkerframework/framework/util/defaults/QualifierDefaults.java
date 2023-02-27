b91ac8c58e8a01f0c59d766b88002eac8dae3809
Suzanne Millstein <smillst@cs.washington.edu>
2023-02-27 13:11:07 -0800

    Don't cache annotatedfor while parsing annotation files

--- framework/src/main/java/org/checkerframework/framework/util/defaults/QualifierDefaults.java
+++ framework/src/main/java/org/checkerframework/framework/util/defaults/QualifierDefaults.java
@@ -661,7 +661,11 @@ public class QualifierDefaults {
       }
     }
 
-    elementAnnotatedFors.put(elt, elementAnnotatedForThisChecker);
+    if (atypeFactory.shouldCache
+        && !atypeFactory.stubTypes.isParsing()
+        && !atypeFactory.ajavaTypes.isParsing()) {
+      elementAnnotatedFors.put(elt, elementAnnotatedForThisChecker);
+    }
 
     return elementAnnotatedForThisChecker;
   }
