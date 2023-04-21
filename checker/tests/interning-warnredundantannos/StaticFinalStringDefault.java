c8c08ef572837045025742102b4a5d1b82161db9
Haifeng Shi <shihaifeng1998@gmail.com>
2023-04-21 12:01:37 -0400

    Update comment in StaticFinalStringDefault.java
    Co-authored-by: Werner Dietl <wdietl@gmail.com>

--- checker/tests/interning-warnredundantannos/StaticFinalStringDefault.java
+++ checker/tests/interning-warnredundantannos/StaticFinalStringDefault.java
@@ -1,6 +1,6 @@
 import org.checkerframework.checker.interning.qual.Interned;
 
 public class StaticFinalStringDefault {
-    // the default type of str should not be @Interned, even though it's later refined to it.
+    // The default type of str is not @Interned, even though it is later refined to it.
     static final @Interned String str = "a";
 }
