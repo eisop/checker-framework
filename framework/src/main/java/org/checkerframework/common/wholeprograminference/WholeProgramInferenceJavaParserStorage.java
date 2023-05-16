15c2a831f2680a6ceee981376dba8b41c3650962
Michael Ernst <mernst@cs.washington.edu>
2023-05-16 13:47:23 -0700

    Reorder a test (#5872)

--- framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInferenceJavaParserStorage.java
+++ framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInferenceJavaParserStorage.java
@@ -760,7 +760,9 @@ public class WholeProgramInferenceJavaParserStorage
               @Nullable @BinaryName String classNameKey,
               @Nullable TypeDeclaration<?> javaParserNode) {
             String className;
-            if (classNameKey == null) {
+            if (classNameKey != null) {
+              className = classNameKey;
+            } else {
               TypeElement classElt = TreeUtils.elementFromDeclaration(tree);
               className = ElementUtils.getBinaryName(classElt);
 
@@ -775,8 +777,6 @@ public class WholeProgramInferenceJavaParserStorage
                     subtypesMap.computeIfAbsent(supertypeName, k -> new TreeSet<>());
                 subtypeSet.add(className);
               }
-            } else {
-              className = classNameKey;
             }
             ClassOrInterfaceAnnos typeWrapper =
                 new ClassOrInterfaceAnnos(className, javaParserNode);
