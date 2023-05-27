b6ba93274be2e7b65a99783a8d1fdd5bcbd9a754
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-05-26 23:22:27 -0400

    change past testcase

--- checker/tests/guieffect/ThrowCatchTest.java
+++ checker/tests/guieffect/ThrowCatchTest.java
@@ -16,15 +16,15 @@ public class ThrowCatchTest {
     // Type var test
     <E extends @UI PolyUIException> void throwTypeVarUI1(E ex1, @UI E ex2) throws PolyUIException {
         if (flag) {
-            // :: error: (throw.type.invalid)
+            // :: error: (throw.type.incompatible)
             throw ex1;
         }
-        // :: error: (throw.type.invalid)
+        // :: error: (throw.type.incompatible)
         throw ex2;
     }
 
     <@UI E extends @UI PolyUIException> void throwTypeVarUI2(E ex1) throws PolyUIException {
-        // :: error: (throw.type.invalid)
+        // :: error: (throw.type.incompatible)
         throw ex1;
     }
 
@@ -47,7 +47,7 @@ public class ThrowCatchTest {
     <@AlwaysSafe E extends @UI PolyUIException> void throwTypeVarMixed(E ex1, @AlwaysSafe E ex2)
             throws PolyUIException {
         if (flag) {
-            // :: error: (throw.type.invalid)
+            // :: error: (throw.type.incompatible)
             throw ex1;
         }
         throw ex2;
@@ -74,7 +74,7 @@ public class ThrowCatchTest {
 
     void throwDeclared() {
         try {
-            // :: error: (throw.type.invalid)
+            // :: error: (throw.type.incompatible)
             throw ui;
         } catch (@UI PolyUIException UIParam) {
 
