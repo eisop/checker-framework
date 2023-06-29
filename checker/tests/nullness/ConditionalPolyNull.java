9f757d719363f3a684bde06c7f543fc6a32b621a
Werner Dietl <wdietl@gmail.com>
2023-06-29 04:42:12 -0400

    Apply suggestions from code review

--- checker/tests/nullness/ConditionalPolyNull.java
+++ checker/tests/nullness/ConditionalPolyNull.java
@@ -1,7 +1,9 @@
+// Test case for EISOP issue #519
+// https://github.com/eisop/checker-framework/issues/519
 import org.checkerframework.checker.nullness.qual.PolyNull;
 
 class ConditionalPolyNull {
-    public static @PolyNull String toLowerCaseA(@PolyNull String text) {
+    @PolyNull String toLowerCaseA(@PolyNull String text) {
         return text == null ? null : text.toLowerCase();
     }
 
