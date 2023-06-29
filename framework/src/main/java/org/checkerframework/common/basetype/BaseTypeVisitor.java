5a5712dea5b0b7612c9bf2b01152279c77c026b8
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-06-28 20:50:20 -0400

    fix conditional polynull

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -2343,10 +2343,12 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      */
     @Override
     public Void visitConditionalExpression(ConditionalExpressionTree tree, Void p) {
-        AnnotatedTypeMirror cond = atypeFactory.getAnnotatedType(tree);
-        this.commonAssignmentCheck(cond, tree.getTrueExpression(), "conditional.type.incompatible");
+        AnnotatedTypeMirror condTrue = atypeFactory.getAnnotatedType(tree);
+        AnnotatedTypeMirror condFalse = atypeFactory.getAnnotatedType(tree);
         this.commonAssignmentCheck(
-                cond, tree.getFalseExpression(), "conditional.type.incompatible");
+                condTrue, tree.getTrueExpression(), "conditional.type.incompatible");
+        this.commonAssignmentCheck(
+                condFalse, tree.getFalseExpression(), "conditional.type.incompatible");
         return super.visitConditionalExpression(tree, p);
     }
 
