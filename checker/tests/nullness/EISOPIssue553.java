eb30d7b5fd056108d52438443ccb8b56793df084
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-08-13 17:09:48 -0400

    add error message

--- checker/tests/nullness/EISOPIssue553.java
+++ checker/tests/nullness/EISOPIssue553.java
@@ -5,6 +5,7 @@ public class EISOPIssue553 {
     public static void main(String[] args) {
         EISOPIssue553 x = null;
         Object o = x.sfield;
+        // :: error: (dereference.of.nullable)
         o = x.field;
     }
 }
