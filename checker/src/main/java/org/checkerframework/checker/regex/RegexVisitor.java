12ed609bdb7f4666e034ab6e57e820cea0ede4b4
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-09-10 01:40:59 -0400

    apply spotless

--- checker/src/main/java/org/checkerframework/checker/regex/RegexVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/regex/RegexVisitor.java
@@ -105,7 +105,8 @@ public class RegexVisitor extends BaseTypeVisitor<RegexAnnotatedTypeFactory> {
                     // When checking implementations of java.util.regex.MatcherResult, calls to
                     // group (and other methods) don't have a receiver tree.  So, just do the
                     // regular checking.
-                    // Verifying an implementation of a subclass of MatcherResult is out of the scope
+                    // Verifying an implementation of a subclass of MatcherResult is out of the
+                    // scope
                     // of this checker.
                     return super.visitMethodInvocation(tree, p);
                 }
