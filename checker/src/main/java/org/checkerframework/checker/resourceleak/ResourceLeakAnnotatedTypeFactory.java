fec408537d3a3cd03b690ca4da032d6509469f97
Michael Ernst <mernst@cs.washington.edu>
2023-09-16 09:16:36 -0700

    Remove unneeded "Logic"

--- checker/src/main/java/org/checkerframework/checker/resourceleak/ResourceLeakAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/resourceleak/ResourceLeakAnnotatedTypeFactory.java
@@ -138,13 +138,12 @@ public class ResourceLeakAnnotatedTypeFactory extends CalledMethodsAnnotatedType
 
     // Inferring owning annotations for final owning fields
     /* NO-AFU
-           if (getWholeProgramInference() != null) {
-               if (cfg.getUnderlyingAST().getKind() == UnderlyingAST.Kind.METHOD) {
-                   MustCallInferenceLogic mustCallInferenceLogic =
-                           new MustCallInferenceLogic(this, cfg);
-                   mustCallInferenceLogic.runInference();
-               }
-           }
+    if (getWholeProgramInference() != null) {
+      if (cfg.getUnderlyingAST().getKind() == UnderlyingAST.Kind.METHOD) {
+        MustCallInference mustCallInferenceLogic = new MustCallInference(this, cfg);
+        mustCallInferenceLogic.runInference();
+      }
+    }
     */
 
     super.postAnalyze(cfg);
