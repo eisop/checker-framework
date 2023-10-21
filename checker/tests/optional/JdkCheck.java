9cfc2a5d141e7cf77b701efddf5321eea72807ae
Michael Ernst <mernst@cs.washington.edu>
2023-10-21 09:44:26 -0700

    Documentation about `Optional.orElseThrow(Supplier)`

--- checker/tests/optional/JdkCheck.java
+++ checker/tests/optional/JdkCheck.java
@@ -21,12 +21,10 @@ public class JdkCheck {
   }
 
   String orElseThrowTest2(Optional<String> mos, Supplier<RuntimeException> exceptionSupplier) {
-    // :: error: (method.invocation.invalid)
     return mos.orElseThrow(exceptionSupplier);
   }
 
   String orElseThrowTestFlow(Optional<String> mos, Supplier<RuntimeException> exceptionSupplier) {
-    // :: error: (method.invocation.invalid)
     mos.orElseThrow(exceptionSupplier);
     return mos.get();
   }
