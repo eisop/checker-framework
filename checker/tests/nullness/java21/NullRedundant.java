38b0d7d39d9f73a6121df22a5193ab1d10792400
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-11-27 15:28:45 -0500

    enhance test case and add annotation to test case for all system

--- checker/tests/nullness/java21/NullRedundant.java
+++ checker/tests/nullness/java21/NullRedundant.java
@@ -1,24 +1,31 @@
 // @below-java21-jdk-skip-test
 
 class NullRedundant {
-    void test(Object o) {
-        // :: (nulltest.redundant)
+    void test1(Object o) {
+        // :: warning: (nulltest.redundant)
         if (o == null) {
             System.out.println("o is null");
         }
         switch(o) {
             case Number n: System.out.println("Number: " + n); break;
-            // :: (nulltest.redundant)
+            // :: warning: (nulltest.redundant)
             case null: System.out.println("null"); break;
             default: System.out.println("anything else");
         }
 
         var output = switch(o) {
             case Number n -> "Number: " + n;
-            // :: (nulltest.redundant)
+            // :: warning: (nulltest.redundant)
             case null -> "null";
             default -> "anything else";
         };
+    }
 
+    Object test2(Object o) {
+        return switch(o) {
+            // :: warning: (nulltest.redundant)
+            case null -> "null";
+            default -> "anything else";
+        };
     }
 }
