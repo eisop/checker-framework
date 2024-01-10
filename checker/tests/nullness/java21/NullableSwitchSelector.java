13ea2adbf5d97a7e2c1c8abbec2062dce0fd33e6
Suzanne Millstein <smillst@cs.washington.edu>
2024-01-10 08:01:31 -0800

    The latest version of Google Java Format formats Java 21 features. (#6386)

--- checker/tests/nullness/java21/NullableSwitchSelector.java
+++ checker/tests/nullness/java21/NullableSwitchSelector.java
@@ -2,7 +2,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 // @below-java21-jdk-skip-test
 
-// None of the WPI formats support the new Java 21 languages features, so skip inference until they
+// None of the WPI formats supports the new Java 21 languages features, so skip inference until they
 // do.
 // @infer-jaifs-skip-test
 // @infer-ajava-skip-test
@@ -13,7 +13,7 @@ public class NullableSwitchSelector {
     return switch (obj) {
       case Integer i -> obj.toString();
       case String s -> String.format("String %s", s);
-      // :: error: (dereference.of.nullable)
+        // :: error: (dereference.of.nullable)
       case null -> obj.toString();
       default -> obj.toString();
     };
@@ -35,7 +35,7 @@ public class NullableSwitchSelector {
     return switch (obj) {
       case Integer i -> obj.toString();
       case String s -> String.format("String %s", s);
-      // :: error: (dereference.of.nullable)
+        // :: error: (dereference.of.nullable)
       case null, default -> obj.toString();
     };
   }
