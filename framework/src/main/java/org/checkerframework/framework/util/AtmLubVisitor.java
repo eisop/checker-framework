d76796de7208a2d59dd80d6d39ad361fd3bbd4cf
Werner Dietl <wdietl@gmail.com>
2024-02-02 16:02:55 -0500

    Work around exception

--- framework/src/main/java/org/checkerframework/framework/util/AtmLubVisitor.java
+++ framework/src/main/java/org/checkerframework/framework/util/AtmLubVisitor.java
@@ -215,6 +215,17 @@ class AtmLubVisitor extends AbstractAtmComboVisitor<Void, AnnotatedTypeMirror> {
 
     private void lubTypeArgument(
             AnnotatedTypeMirror type1, AnnotatedTypeMirror type2, AnnotatedTypeMirror lub) {
+        if ((type1.getKind() == TypeKind.WILDCARD
+                        && ((AnnotatedWildcardType) type1).isUninferredTypeArgument())
+                || (type2.getKind() == TypeKind.WILDCARD
+                        && ((AnnotatedWildcardType) type2).isUninferredTypeArgument())) {
+            // The asSuper calls below don't seem to retain if a type variable was uninferred.
+            // There is a similar check in the wildcards branch below, not sure when that is
+            // actually hit.
+            // TODO: see whether anything else should be done. See issue 6438.
+            return;
+        }
+
         // In lub(), asSuper is called on type1 and type2, but asSuper does not recur into type
         // arguments, so call asSuper on the type arguments so that they have the same underlying
         // type.
