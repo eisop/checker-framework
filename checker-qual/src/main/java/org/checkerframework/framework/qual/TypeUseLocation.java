b564e37f6b88819a2e1fac54b9bda9173ad45395
Werner Dietl <wdietl@gmail.com>
2024-02-27 22:26:14 -0500

    Fix javadoc

--- checker-qual/src/main/java/org/checkerframework/framework/qual/TypeUseLocation.java
+++ checker-qual/src/main/java/org/checkerframework/framework/qual/TypeUseLocation.java
@@ -130,9 +130,9 @@ public enum TypeUseLocation {
     /**
      * Apply default annotations to unannotated type variable uses: {@code T}.
      *
-     * <p>To get parametric polymorphism: add {@link ParametricTypeVariableUse} to your type system
-     * and use it as default for {@code TYPE_VARIABLE_USE}, which is treated like no annotation on
-     * the type variable use.
+     * <p>To get parametric polymorphism: add a qualifier that is meta-annotated with {@link
+     * ParametricTypeVariableUseQualifier} to your type system and use it as default for {@code
+     * TYPE_VARIABLE_USE}, which is treated like no annotation on the type variable use.
      */
     TYPE_VARIABLE_USE,
 
