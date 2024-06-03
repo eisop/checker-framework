14e3a276a75523e674026f34ff5f124f2eacbed9
Michael Ernst <mernst@cs.washington.edu>
2024-06-03 06:07:50 -0700

    Don't use `-XDrawDiagnostics` in testing framework (#6647)

--- framework-test/src/main/java/org/checkerframework/framework/test/TestConfigurationBuilder.java
+++ framework-test/src/main/java/org/checkerframework/framework/test/TestConfigurationBuilder.java
@@ -63,7 +63,6 @@ public class TestConfigurationBuilder {
             .addOption("-g")
             .addOption("-Xlint:unchecked")
             .addOption("-Xlint:deprecation")
-            .addOption("-XDrawDiagnostics") // use short javac diagnostics
             .addOption("-ApermitMissingJdk")
             .addOption("-AnoJreVersionCheck");
 
