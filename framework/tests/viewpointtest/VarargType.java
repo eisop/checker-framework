14f4c61dae550500bb33fa4daa17c0ac36dcf661
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-06-20 20:24:52 -0400

    More test cases for VarargsConstructor

--- framework/tests/viewpointtest/VarargType.java
+++ framework/tests/viewpointtest/VarargType.java
@@ -1,27 +1,46 @@
+// Test case for EISOP issue #777:
+// https://github.com/eisop/checker-framework/issues/777
 import viewpointtest.quals.*;
 
-public class VarargType {
+public class VarargsConstructor {
 
-    VarargType(String str, Object... args) {}
+    VarargsConstructor(String str, Object... args) {}
+
+    @SuppressWarnings({"inconsistent.constructor.type", "super.invocation.invalid"})
+    @ReceiverDependentQual
+    VarargsConstructor(@ReceiverDependentQual Object... args) {}
 
     void foo() {
-        VarargType a = new VarargType("testStr", new Object());
+        VarargsConstructor a = new VarargsConstructor("testStr", new Object());
+    }
+
+    void invokeConstructor(@A Object aObj, @B Object bObj, @Top Object topObj) {
+        new @A VarargsConstructor(aObj);
+        new @B VarargsConstructor(bObj);
+        new @Top VarargsConstructor(topObj);
+        // :: error: (argument.type.incompatible)
+        new @A VarargsConstructor(bObj);
+        // :: error: (argument.type.incompatible)
+        new @B VarargsConstructor(aObj);
     }
 
     // inner class
     class Inner {
-        Inner(String str, Object... args) {}
+        Inner(Object... args) {}
 
         void foo() {
-            Inner a = new Inner("testStr", new Object());
+            Inner a = new Inner();
+            Inner b = new Inner(new Object());
+            Inner c = VarargsConstructor.this.new Inner();
+            Inner d = VarargsConstructor.this.new Inner(new Object());
         }
     }
 
     // anonymous class
     Object o =
-            new Object() {
+            new VarargType("testStr", new Object()) {
                 void foo() {
-                    VarargType a = new VarargType("testStr", new Object());
+                    VarargsConstructor a = new VarargsConstructor("testStr", new Object());
                 }
             };
 }
