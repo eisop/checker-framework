884a1ec7975f9d90a239d1a3b709f095ad0503e6
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-06-21 18:34:21 -0400

    Update the test case with actual VPA for inner and anonymous class

--- framework/tests/viewpointtest/VarargsConstructor.java
+++ framework/tests/viewpointtest/VarargsConstructor.java
@@ -25,7 +25,9 @@ public class VarargsConstructor {
     }
 
     class Inner {
-        Inner(Object... args) {}
+        @SuppressWarnings({"inconsistent.constructor.type", "super.invocation.invalid"})
+        @ReceiverDependentQual
+        Inner(@ReceiverDependentQual Object... args) {}
 
         void foo() {
             Inner a = new Inner();
@@ -33,13 +35,31 @@ public class VarargsConstructor {
             Inner c = VarargsConstructor.this.new Inner();
             Inner d = VarargsConstructor.this.new Inner(new Object());
         }
+
+        void invokeConstructor(@A Object aObj, @B Object bObj, @Top Object topObj) {
+            @A Object a = new @A Inner(aObj);
+            @B Object b = new @B Inner(bObj);
+            @Top Object top = new @Top Inner(topObj);
+            // :: error: (argument.type.incompatible)
+            new @A Inner(bObj);
+            // :: error: (argument.type.incompatible)
+            new @B Inner(aObj);
+        }
     }
 
-    // anonymous class
-    Object o =
-            new VarargsConstructor("testStr", new Object()) {
-                void foo() {
-                    VarargsConstructor a = new VarargsConstructor("testStr", new Object());
-                }
-            };
+    void testAnonymousClass(@A Object aObj, @B Object bObj, @Top Object topObj) {
+        Object o =
+                new VarargsConstructor("testStr", new Object()) {
+                    void foo() {
+                        VarargsConstructor a = new VarargsConstructor("testStr", new Object());
+                    }
+                };
+        @A Object a = new @A VarargsConstructor(aObj) {};
+        @B Object b = new @B VarargsConstructor(bObj) {};
+        @Top Object top = new @Top VarargsConstructor(topObj) {};
+        // :: error: (argument.type.incompatible)
+        new @A VarargsConstructor(bObj) {};
+        // :: error: (argument.type.incompatible)
+        new @B VarargsConstructor(aObj) {};
+    }
 }
