4702169934df2c8d84d2c18c73b00291e99941de
James Yoo <24359440+jyoo980@users.noreply.github.com>
2024-06-25 12:17:27 -0400

    Non-Empty Checker (#6679)
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>
    Co-authored-by: Suzanne Millstein <smillst@cs.washington.edu>

--- framework/src/main/java/org/checkerframework/framework/util/AnnotatedTypes.java
+++ framework/src/main/java/org/checkerframework/framework/util/AnnotatedTypes.java
@@ -1026,6 +1026,7 @@ public class AnnotatedTypes {
       Tree invok) {
 
     List<AnnotatedTypeMirror> parameters = method.getParameterTypes();
+
     // Handle anonymous constructors that extend a class with an enclosing type,
     // as in `new MyClass(){ ... }`.
     if (method.getElement().getKind() == ElementKind.CONSTRUCTOR
