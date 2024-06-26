c63b87c76665268da31154836f040acdd92f0282
Werner Dietl <wdietl@gmail.com>
2024-06-25 21:51:12 -0400

    Undo change to broken Travis setup

--- .travis-build.sh
+++ .travis-build.sh
@@ -36,7 +36,7 @@ ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SCRIPTDIR=$ROOTDIR/checker/bin-devel/
 
 # For debugging
-(cd "$CHECKERFRAMEWORK" && ./gradlew getGitScripts)
+(cd "$CHECKERFRAMEWORK" && ./gradlew getPlumeScripts)
 "${SCRIPTDIR}/plume-scripts/ci-info" eisop
 eval $("${SCRIPTDIR}/plume-scripts/ci-info" eisop)
 
