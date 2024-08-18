9c4c12adcf13eb7cc1fa62f0f56f6d6e866c432f
Werner Dietl <wdietl@gmail.com>
2024-08-17 20:05:54 -0400

    Apply suggestions from code review

--- checker/bin-devel/test-cftests-nonjunit.sh
+++ checker/bin-devel/test-cftests-nonjunit.sh
@@ -10,7 +10,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1090# In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
 source "$SCRIPTDIR"/clone-related.sh
 
-# Adding --max-workers=1 to avoid random failures in Github Actions. alternative solution is to use --no-build-cache.
+# Adding --max-workers=1 to avoid random failures in Github Actions. An alternative solution is to use --no-build-cache.
 # https://github.com/eisop/checker-framework/issues/849
 ./gradlew nonJunitTests -x javadoc -x allJavadoc --console=plain --warning-mode=all --max-workers=1
 ./gradlew publishToMavenLocal -x javadoc -x allJavadoc --console=plain --warning-mode=all
