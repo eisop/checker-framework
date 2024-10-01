398ca37a0e1a88e2c96a6d9e0a4f2154561ba0b0
Alex Cook <a5cook@uwaterloo.ca>
2024-10-01 17:40:44 -0400

    collapsed main modifier test case into nullness test suite

--- checker/tests/nullness/ArrayMainModifierNullableAnnotation.java
+++ checker/tests/nullness/ArrayMainModifierNullableAnnotation.java
@@ -1,6 +1,6 @@
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class NonNullElementAnnotation {
+public class ArrayMainModifierNullableAnnotation {
     void foo() {
         // :: warning: (new.array.nullable.ignored)
         int[] o = new int @Nullable [10];
