43cd599d2b3a93af7dd26af09612ce21fac1f840
Michael Ernst <mernst@cs.washington.edu>
2024-10-28 09:00:17 -0700

    Don't re-compute the enclosing method (#6876)

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -2414,9 +2414,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
 
     AnnotatedTypeMirror ret = null;
     if (enclosing.getKind() == Tree.Kind.METHOD) {
-
-      MethodTree enclosingMethod = TreePathUtil.enclosingMethod(getCurrentPath());
-      boolean valid = validateTypeOf(enclosing);
+      MethodTree enclosingMethod = (MethodTree) enclosing;
+      boolean valid = validateTypeOf(enclosingMethod);
       if (valid) {
         ret = atypeFactory.getMethodReturnType(enclosingMethod, tree);
       }
