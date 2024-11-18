8d09f9db012b205c9e2206a1974c0fcfab00961d
Aosen Xiong <asxiong@amazon.com>
2024-11-17 21:09:57 -0800

    Line 43 issue error because `initialization` keyword does not work

---
+++ checker/src/test/java/org/checkerframework/checker/test/junit/AnnotatedForNullnessTest.java
@@ -0,0 +1,25 @@
+package org.checkerframework.checker.test.junit;
+
+import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
+import org.junit.runners.Parameterized.Parameters;
+import java.io.File;
+import java.util.List;
+
+public class AnnotatedForNullnessTest extends CheckerFrameworkPerDirectoryTest {
+
+    /**
+     * @param testFiles the files containing test code, which will be type-checked
+     */
+    public AnnotatedForNullnessTest(List<File> testFiles) {
+        super(
+                testFiles,
+                org.checkerframework.checker.nullness.NullnessChecker.class,
+                "nullness",
+                "-AuseConservativeDefaultsForUncheckedCode=source,bytecode");
+    }
+
+    @Parameters
+    public static String[] getTestDirs() {
+        return new String[] {"nulless-conservative-defaults/annotatedfornullness"};
+    }
+}
