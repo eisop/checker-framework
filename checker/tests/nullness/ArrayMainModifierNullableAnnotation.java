8ed73ac9a29df4e5400c317d7d9c99c725345466
Alex Cook <a5cook@uwaterloo.ca>
2024-11-25 16:57:58 -0500

    changed message key and consolidated test cases

--- checker/tests/nullness/ArrayMainModifierNullableAnnotation.java
+++
@@ -1,8 +0,0 @@
-import org.checkerframework.checker.nullness.qual.Nullable;
-
-public class ArrayMainModifierNullableAnnotation {
-    void foo() {
-        // :: warning: (new.array.nullable.ignored)
-        int[] o = new int @Nullable [10];
-    }
-}
