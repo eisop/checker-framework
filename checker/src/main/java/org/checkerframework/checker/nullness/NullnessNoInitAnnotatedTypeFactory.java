536cadb285c62c655d8a41faa30e1147dd8d3068
Alex Cook <a5cook@uwaterloo.ca>
2024-12-18 18:48:34 -0500

    check ATF newClass visitor for explicit annotations and cache new class trees in AnnotatedTypeFactory

--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessNoInitAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessNoInitAnnotatedTypeFactory.java
@@ -734,26 +734,24 @@ public class NullnessNoInitAnnotatedTypeFactory
         // explicit nullable annotations are left intact for the visitor to inspect.
         @Override
         public Void visitNewClass(NewClassTree tree, AnnotatedTypeMirror type) {
-            type.addMissingAnnotation(NONNULL);
-            return null;
-        }
 
-        @Override
-        public Void visitMethod(MethodTree tree, AnnotatedTypeMirror type) {
-
-            if (type instanceof AnnotatedExecutableType) {
-                AnnotatedExecutableType executableType = (AnnotatedExecutableType) type;
-
-                if (TreeUtils.isConstructor(tree)) {
-                    AnnotatedTypeMirror returnType = executableType.getReturnType();
-                    if (returnType != null) {
-                        returnType.addMissingAnnotation(NONNULL);
-                    }
+            Tree identifier = tree.getIdentifier();
+            if (identifier != null) {
+                String identifierString = identifier.toString();
+                // look at identifier as string to see what annotations programmer explicitly wrote
+                if (identifierString.contains("@Nullable")
+                        || identifierString.contains("@MonotonicNonNull")
+                        || identifierString.contains("@PolyNull")) {
+                    type.addMissingAnnotation(NONNULL);
+                } else {
+                    type.replaceAnnotation(NONNULL);
                 }
             }
             return null;
         }
 
+        // The result of newly allocated structures is always non-null,
+        // explicit nullable annotations are left intact for the visitor to inspect.
         @Override
         public Void visitNewArray(NewArrayTree tree, AnnotatedTypeMirror type) {
             super.visitNewArray(tree, type);
