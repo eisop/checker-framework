0d7b24603001542740403ec8f1b13f616b75e390
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-12-21 01:54:10 -0500

    Remove link and uniform the text

--- framework/src/test/java/viewpointtest/quals/ReceiverDependentQual.java
+++ framework/src/test/java/viewpointtest/quals/ReceiverDependentQual.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** The {@link ReceiverDependentQual} qualifier. */
+/** The ReceiverDependentQual qualifier. */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
