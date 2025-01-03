c165f4c204106531af667252e5d455a230f635cc
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-01-02 16:55:11 -0800

    Update Javadoc link based on previous observation for JDK8

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -1679,8 +1679,9 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      *
      * <p>The other two validate methods achieve the same goal but perform checks on different trees
      * and different type-use locations. See {@link BaseTypeVisitor#validateTargetLocation(Tree,
-     * AnnotatedTypeMirror, TypeUseLocation)} and
-     * BaseTypeValidator#validateWildCardTargetLocation(AnnotatedWildcardType, Tree).
+     * AnnotatedTypeMirror, TypeUseLocation)} and {@link
+     * BaseTypeValidator#validateWildCardTargetLocation(AnnotatedTypeMirror.AnnotatedWildcardType,
+     * Tree)}.
      *
      * @param tree qualifiers on this VariableTree will be validated
      * @param type the type of the tree
@@ -1762,7 +1763,9 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * <p>The other two validate methods achieve the same goal but perform checks on different trees
      * and different type-use locations. See {@link
      * BaseTypeVisitor#validateVariablesTargetLocation(VariableTree, AnnotatedTypeMirror)} and
-     * BaseTypeValidator#validateWildCardTargetLocation(AnnotatedWildcardType, Tree).
+     * {@link
+     * BaseTypeValidator#validateWildCardTargetLocation(AnnotatedTypeMirror.AnnotatedWildcardType,
+     * Tree)}.
      *
      * @param tree qualifiers on the tree will be validated
      * @param type the type of the tree
