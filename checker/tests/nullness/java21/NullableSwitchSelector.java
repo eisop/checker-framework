f69173b362f293609bcf87ada82de6d3c2039d6e
Werner Dietl <wdietl@gmail.com>
2025-01-29 19:47:01 -0500

    Switch back to newest version of GJF

--- checker/tests/nullness/java21/NullableSwitchSelector.java
+++ checker/tests/nullness/java21/NullableSwitchSelector.java
@@ -13,7 +13,7 @@ public class NullableSwitchSelector {
     return switch (obj) {
       case Integer i -> obj.toString();
       case String s -> String.format("String %s", s);
-        // :: error: (dereference.of.nullable)
+      // :: error: (dereference.of.nullable)
       case null -> obj.toString();
       default -> obj.toString();
     };
@@ -35,7 +35,7 @@ public class NullableSwitchSelector {
     return switch (obj) {
       case Integer i -> obj.toString();
       case String s -> String.format("String %s", s);
-        // :: error: (dereference.of.nullable)
+      // :: error: (dereference.of.nullable)
       case null, default -> obj.toString();
     };
   }
