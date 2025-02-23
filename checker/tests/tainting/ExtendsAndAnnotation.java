3e2d765ebd35927857f872f37a239b9ad8d7121b
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-02-23 13:47:16 -0500

    Allow supertype annotation for tainting checker

--- checker/tests/tainting/ExtendsAndAnnotation.java
+++ checker/tests/tainting/ExtendsAndAnnotation.java
@@ -5,7 +5,6 @@ import org.checkerframework.checker.tainting.qual.Tainted;
 import org.checkerframework.checker.tainting.qual.Untainted;
 import org.checkerframework.framework.qual.HasQualifierParameter;
 
-// :: error: (annotation.on.supertype)
 public class ExtendsAndAnnotation extends @Tainted Object {
     void test(@Untainted ExtendsAndAnnotation c) {
         // :: warning: (cast.unsafe.constructor.invocation)
@@ -16,6 +15,5 @@ public class ExtendsAndAnnotation extends @Tainted Object {
     @HasQualifierParameter(Tainted.class)
     // :: error: (invalid.polymorphic.qualifier)
     // :: error: (declaration.inconsistent.with.extends.clause)
-    // :: error: (annotation.on.supertype)
     static class Banana extends @PolyTainted Object {}
 }
