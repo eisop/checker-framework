1fa2d52df4cc61b919d866ecb2f10c8dc91d51f4
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-03-11 00:18:09 -0400

    Move test to misc test

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -70,3 +70,36 @@ git diff --exit-code docs/manual/contributors.tex || \
      echo "    https://github.com/eisop-plume-lib/git-scripts/blob/master/git-authors.sed" &&
      echo "    and remake contributors.tex after that pull request is merged." &&
      false)
+
+# Check the definition of qualifiers in Checker Framework against the JDK
+set +o xtrace
+current_path=$(pwd)
+src_dir="$current_path/checker-qual/src/main/java/org/checkerframework"
+jdk_dir="$current_path/../jdk/src/java.base/share/classes/org/checkerframework"
+
+difference_found=false
+
+for file in $(find "$src_dir" -name "*.java"); do
+    rel_path="${file#"$src_dir"/}"
+    jdk_file="$jdk_dir/$rel_path"
+
+    if [ -f "$jdk_file" ]; then
+        diff_output=$(diff -q "$file" "$jdk_file" || true)
+
+        if [ "$diff_output" ]; then
+            echo "Difference found in: $rel_path"
+            diff "$file" "$jdk_file" || true  # Show the full diff
+
+            difference_found=true
+        fi
+    fi
+done
+
+# If any difference was found, exit with a non-zero status
+if [ "$difference_found" = true ]; then
+    echo "Differences found. Exiting with failure."
+    exit 1  # Exit with failure
+else
+    echo "No differences found."
+    exit 0  # Exit with success
+fi
