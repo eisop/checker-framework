0f469c96a8729eedb876a851c760bef322954a7c
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-03-13 01:38:35 -0400

    Build CF in the script

--- checker/bin-devel/test-typecheck-jdk.sh
+++ checker/bin-devel/test-typecheck-jdk.sh
@@ -9,6 +9,10 @@ echo "SHELLOPTS=${SHELLOPTS}"
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SCRIPTDIR"/clone-related.sh
 
+# Run assembleForJavac because it does not build the javadoc, so it is faster than assemble.
+echo "running \"./gradlew assembleForJavac\" for checker-framework"
+./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
+
 ./checker/bin/javac -processor nullness \
   --patch-module java.base=../jdk/src/java.base/share/classes \
   -Xmaxerrs 5000 \
