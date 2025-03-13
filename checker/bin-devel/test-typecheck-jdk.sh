e607f5d8d4e2538c421e696d9425ed3a8bc7657c
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-03-13 01:29:31 -0400

    Typecheck JDK using script

---
+++ checker/bin-devel/test-typecheck-jdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+# set -o verbose
+set -o xtrace
+export SHELLOPTS
+echo "SHELLOPTS=${SHELLOPTS}"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$SCRIPTDIR"/clone-related.sh
+
+./checker/bin/javac -processor nullness \
+  --patch-module java.base=../jdk/src/java.base/share/classes \
+  -Xmaxerrs 5000 \
+  -nowarn \
+  $(find ../jdk/src/java.base/share/classes/java/util -maxdepth 1 -name '*.java') \
