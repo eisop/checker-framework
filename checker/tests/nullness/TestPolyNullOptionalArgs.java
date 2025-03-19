91230783b06c6fb99f358fef2cf00a0a54f59517
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-03-19 12:40:19 -0400

    Further expand the test cases with generics

--- checker/tests/nullness/TestPolyNullOptionalArgs.java
+++ checker/tests/nullness/TestPolyNullOptionalArgs.java
@@ -1,6 +1,8 @@
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.checker.nullness.qual.PolyNull;
 
+import java.util.List;
+
 public class TestPolyNullOptionalArgs {
 
     public static @PolyNull("elt") Object @PolyNull("container") [] identity(
@@ -8,7 +10,7 @@ public class TestPolyNullOptionalArgs {
         return seq;
     }
 
-    void test(
+    void testArray(
             @Nullable Object[] arr1,
             @Nullable Object @Nullable [] arr2,
             Object[] arr3,
@@ -47,5 +49,57 @@ public class TestPolyNullOptionalArgs {
         // :: error: (assignment.type.incompatible)
         arr4 = identity(arr2);
         arr4 = identity(arr3);
+    }
+
+    @SuppressWarnings("return.type.incompatible")
+    public static <E> @PolyNull("elt") Object @PolyNull("container") [] toArray(
+            @PolyNull("container") List<@PolyNull("elt") E> list) {
+        return null;
+    }
+
+    void testList(
+            List<@Nullable Object> list1,
+            @Nullable List<@Nullable Object> list2,
+            List<Object> list3,
+            @Nullable List<Object> list4,
+            @Nullable Object[] arr1,
+            @Nullable Object @Nullable [] arr2,
+            Object[] arr3,
+            Object @Nullable [] arr4) {
+        arr1 = toArray(list1);
+        // lhs: @Nullable Object @Nonnull []; rhs input: @Nullable List<@Nullable Object>
+        // :: error: (assignment.type.incompatible)
+        arr1 = toArray(list2);
+        // lhs: @Nullable Object @Nonnull []; rhs input: @Nonnull List<@Nonnull Object>
+        arr1 = toArray(list3);
+        // lhs: @Nullable Object @Nonnull []; rhs input: @Nullable List<@Nonnull Object>
+        // :: error: (assignment.type.incompatible)
+        arr1 = toArray(list4);
+        // lhs: @Nullable Object @Nullable []; rhs input: @Nullable List<@Nullable Object>
+        arr2 = toArray(list2);
+        // lhs: @Nullable Object @Nullable []; rhs input: @Nonnull List<@Nullable Object>
+        arr2 = toArray(list1);
+        // lhs: @Nullable Object @Nullable []; rhs input: @Nonnull List<@Nonnull Object>
+        arr2 = toArray(list3);
+        // lhs: @Nullable Object @Nullable []; rhs input: @Nullable List<@Nonnull Object>
+        arr2 = toArray(list4);
+        arr3 = toArray(list3);
+        // lhs: @Nonnull Object @Nonnull []; rhs input: @Nonnull List<@Nullable Object>
+        // :: error: (assignment.type.incompatible)
+        arr3 = toArray(list1);
+        // lhs: @Nonnull Object @Nonnull []; rhs input: @Nullable List<@Nullable Object>
+        // :: error: (assignment.type.incompatible)
+        arr3 = toArray(list2);
+        // lhs: @Nonnull Object @Nonnull []; rhs input: @Nullable List<@Nonnull Object>
+        // :: error: (assignment.type.incompatible)
+        arr3 = toArray(list4);
+        arr4 = toArray(list4);
+        // lhs: @Nonnull Object @Nullable []; rhs input: @Nonnull List<@Nullable Object>
+        // :: error: (assignment.type.incompatible)
+        arr4 = toArray(list1);
+        // lhs: @Nonnull Object @Nullable []; rhs input: @Nullable List<@Nullable Object>
+        // :: error: (assignment.type.incompatible)
+        arr4 = toArray(list2);
+        arr4 = toArray(list3);
     }
 }
