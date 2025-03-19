573322f0d7ab399a0b9f70f749ab1d56ce9f22f9
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-03-19 12:21:05 -0400

    Expand test cases

--- checker/tests/nullness/TestPolyNullOptionalArgs.java
+++ checker/tests/nullness/TestPolyNullOptionalArgs.java
@@ -1,4 +1,5 @@
-import org.checkerframework.checker.nullness.qual.*;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.nullness.qual.PolyNull;
 
 public class TestPolyNullOptionalArgs {
 
@@ -13,10 +14,38 @@ public class TestPolyNullOptionalArgs {
             Object[] arr3,
             Object @Nullable [] arr4) {
         arr1 = identity(arr1);
+        // lhs: @Nullable Object @Nonnull []; rhs: @Nullable Object @Nullable []
         // :: error: (assignment.type.incompatible)
         arr1 = identity(arr2);
+        // lhs: @Nullable Object @Nonnull []; rhs: @Nonnull Object @Nonnull []
+        arr1 = identity(arr3);
+        // lhs: @Nullable Object @Nonnull []; rhs: @Nonnull Object @Nullable []
+        // :: error: (assignment.type.incompatible)
+        arr1 = identity(arr4);
         arr2 = identity(arr2);
+        // lhs: @Nullable Object @Nullable []; rhs: @Nullable Object @Nonnull []
+        arr2 = identity(arr1);
+        // lhs: @Nullable Object @Nullable []; rhs: @Nonnull Object @Nonnull []
+        arr2 = identity(arr3);
+        // lhs: @Nullable Object @Nullable []; rhs: @Nonnull Object @Nullable []
+        arr2 = identity(arr4);
         arr3 = identity(arr3);
+        // lhs: @Nonnull Object @Nonnull []; rhs: @Nullable Object @Nonnull []
+        // :: error: (assignment.type.incompatible)
+        arr3 = identity(arr1);
+        // lhs: @Nonnull Object @Nonnull []; rhs: @Nullable Object @Nullable []
+        // :: error: (assignment.type.incompatible)
+        arr3 = identity(arr2);
+        // lhs: @Nonnull Object @Nonnull []; rhs: @Nonnull Object @Nullable []
+        // :: error: (assignment.type.incompatible)
+        arr3 = identity(arr4);
         arr4 = identity(arr4);
+        // lhs: @Nonnull Object @Nullable []; rhs: @Nullable Object @Nonnull []
+        // :: error: (assignment.type.incompatible)
+        arr4 = identity(arr1);
+        // lhs: @Nonnull Object @Nullable []; rhs: @Nullable Object @Nullable []
+        // :: error: (assignment.type.incompatible)
+        arr4 = identity(arr2);
+        arr4 = identity(arr3);
     }
 }
