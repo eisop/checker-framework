fc2218a8db0739931bc7f3b56bfb065f3bdff78b
xiayulin123 <xiayulinsky@gmail.com>
2025-03-29 12:23:32 -0400

    added NullawayWithEISOP example

--- docs/examples/NullAwayWithEISOP/src/main/java/com/example/Demo.java
+++ docs/examples/NullAwayWithEISOP/src/main/java/com/example/Demo.java
@@ -2,17 +2,17 @@ package com.example;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public class NullawayWithEISOP {
+public class Demo {
 
     public String generateGreeting(@NonNull String name) {
         if (name.isEmpty()) {
-            return "Hello, stranger!";
+            return "Hello, World!";
         }
         return "Hello, " + name;
     }
 
     public static void main(String[] args) {
-        NullawayWithEISOP test = new NullawayWithEISOP();
+        Demo test = new Demo();
         String greeting = test.generateGreeting(null);
         System.out.println(greeting);
     }
