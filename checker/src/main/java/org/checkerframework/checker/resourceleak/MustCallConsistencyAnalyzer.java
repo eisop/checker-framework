0ffa006bbf45be8345b8dd2cdf3e11e9c0219dc9
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-04-29 21:23:06 -0400

    Undo change

--- checker/src/main/java/org/checkerframework/checker/resourceleak/MustCallConsistencyAnalyzer.java
+++ checker/src/main/java/org/checkerframework/checker/resourceleak/MustCallConsistencyAnalyzer.java
@@ -226,7 +226,7 @@ public class MustCallConsistencyAnalyzer {
      * alias is certainly a null pointer, or if the must-call obligation is the empty set), the
      * analysis can discard the Obligation.
      */
-    public static class Obligation {
+    /*package-private*/ static class Obligation {
 
         /**
          * The set of resource aliases through which a must-call obligation can be satisfied.
