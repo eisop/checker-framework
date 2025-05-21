e6e92d4bbf746f18628c453a81442a08cf4a1133
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-05-21 11:04:23 -0400

    Remove code no longer need

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -3854,19 +3854,13 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
         }
 
         AnnotatedTypeMirror methodReceiver = method.getReceiverType();
-        //        AnnotatedTypeMirror treeReceiverCopy = methodReceiver.shallowCopy(false);
         AnnotatedTypeMirror treeReceiver = atypeFactory.getReceiverType(tree);
 
-        //        treeReceiverCopy.addAnnotations(treeReceiver.getEffectiveAnnotations());
-
         if (!skipReceiverSubtypeCheck(tree, methodReceiver, treeReceiver)) {
-            // The diagnostic can be a bit misleading because the check is of the receiver but
-            // `tree` is the entire method invocation (where the receiver might be implicit).
             commonAssignmentCheckStartDiagnostic(methodReceiver, treeReceiver, tree);
             boolean success = typeHierarchy.isSubtype(treeReceiver, methodReceiver);
             commonAssignmentCheckEndDiagnostic(success, null, methodReceiver, treeReceiver, tree);
             if (!success) {
-                // Don't report the erased types because they show up with '</*RAW*/>' as type args.
                 reportMethodInvocabilityError(tree, treeReceiver, methodReceiver);
             }
         }
