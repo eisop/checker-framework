ff3892b8e1bf48ba4ede9b96965cb36999c5ce3e
Alex Cook <a5cook@uwaterloo.ca>
2025-05-23 12:16:25 -0400

    refactor: reduce inheritDeclAnnoPersist test cases to stubs

--- checker/jtreg/nullness/inheritDeclAnnoPersist25/Implements25.java
+++ checker/jtreg/nullness/inheritDeclAnnoPersist25/Implements25.java
@@ -3,29 +3,7 @@
  * @summary Test that inherited declaration annotations are stored in bytecode.
  *
  * @requires jdk.version.major >= 25
- * @compile ../PersistUtil25.java Driver.java ReferenceInfoUtil.java Implements25.java ../inheritDeclAnnoPersist/AbstractClass.java
- * @run main Driver Implements25
+ * @compile ../PersistUtil25.java Driver.java ReferenceInfoUtil.java ../inheritDeclAnnoPersist/Implements.java ../inheritDeclAnnoPersist/AbstractClass.java
+ * @run main Driver Implements
+ *
  */
-
-public class Implements25 {
-
-    @ADescriptions({
-        @ADescription(annotation = "org/checkerframework/checker/nullness/qual/EnsuresNonNull")
-    })
-    public String m1() {
-        return TestWrapper.wrap(
-                "public Test() { f = new Object(); }",
-                "@Override public void setf() { f = new Object(); }",
-                "@Override public void setg() {}");
-    }
-}
-
-class TestWrapper {
-    public static String wrap(String... method) {
-        return String.join(
-                System.lineSeparator(),
-                "class Test extends AbstractClass {",
-                String.join(System.lineSeparator(), method),
-                "}");
-    }
-}
