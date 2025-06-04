651af78478e79bc2248d88bdd3c3b749666dc67b
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-06-04 17:05:12 -0400

    Format; Improve the tests

--- checker/jtreg/Lintoption/InstanceofLintOptionDisabled.java
+++ checker/jtreg/Lintoption/InstanceofLintOptionDisabled.java
@@ -3,7 +3,8 @@
  * @summary Test case for instanceof lint option: -Alint=-instanceof
  * @requires jdk.version >= 17
  * @compile -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=-instanceof
-  * @compile -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=-instanceof:unsafe
+ * @compile -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=-instanceof:unsafe
+ * @compile -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=instanceof, -instanceof::unsafe
  */
 
 import org.checkerframework.checker.tainting.qual.Untainted;
