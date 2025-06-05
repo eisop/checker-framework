6e44d24e15b34422898cf822382cc86cbbe1b367
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-06-04 20:49:04 -0400

    Fix test case format

--- checker/jtreg/Lintoption/InstanceofLintOptionEnabled.java
+++ checker/jtreg/Lintoption/InstanceofLintOptionEnabled.java
@@ -5,7 +5,7 @@
  * @compile/ref=InstanceofLintOptionEnabled.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java
  * @compile/ref=InstanceofLintOptionEnabled.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=instanceof
  * @compile/ref=InstanceofLintOptionEnabled.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=instanceof:unsafe
- * @compile/ref=InstanceofLintOptionEnabled.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=-instanceof, instanceof::unsafe
+ * @compile/ref=InstanceofLintOptionEnabled.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker InstanceofLintOptionEnabled.java -Alint=-instanceof,instanceof::unsafe
  */
 
 import org.checkerframework.checker.tainting.qual.Untainted;
