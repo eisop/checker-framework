990c16ce01884d146f781970260764b392d1ca82
Billy Bai <y5bai@uwaterloo.ca>
2025-06-08 20:36:04 -0400

    Add comments.

--- framework/tests/h1h2checker/Issue412.java
+++ framework/tests/h1h2checker/Issue412.java
@@ -1,5 +1,7 @@
 import org.checkerframework.framework.testchecker.h1h2checker.quals.*;
 
+// @skip-test
+
 public class Issue412 {
 
     @H1Bot Issue412() {
@@ -7,7 +9,12 @@ public class Issue412 {
     }
 
     class Inner {
+        /* The framework can correctly identify the outer class reciever type to be @H1Top.
+        This rules out the case that this issue is caused by the general framework. */
+        // :: warning : (inconsistent.constructor.type) :: error : (super.invocation.invalid)
         Inner(@H1Top Issue412 Issue412.this) {
+
+            // :: error : (assignment.type.incompatible)
             @H1Bot Issue412 object = Issue412.this;
         }
     }
