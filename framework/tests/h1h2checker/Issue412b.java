cffd968f5a906a4b29cdcc6319e925a66db25115
Billy Bai <y5bai@uwaterloo.ca>
2025-06-10 15:23:24 -0400

    fix typo in the test cases

--- framework/tests/h1h2checker/Issue412b.java
+++ framework/tests/h1h2checker/Issue412b.java
@@ -11,10 +11,10 @@ public class Issue412b {
     class Inner {
         /* The framework can correctly identify the outer class reciever type to be @H1Top.
         This rules out the case that this issue is caused by the general framework. */
-        // :: warning : (inconsistent.constructor.type) :: error : (super.invocation.invalid)
+        // :: warning: (inconsistent.constructor.type) :: error: (super.invocation.invalid)
         Inner(@H1Top Issue412b Issue412b.this) {
 
-            // :: error : (assignment.type.incompatible)
+            // :: error: (assignment.type.incompatible)
             @H1Bot Issue412b object = Issue412b.this;
         }
     }
