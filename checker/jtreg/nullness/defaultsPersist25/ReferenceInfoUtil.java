0b877e05c379f66e63b2ef1e8e14e84bd017a509
Alex Cook <a5cook@uwaterloo.ca>
2025-06-10 14:46:23 -0400

    refactor: remove * imports

--- checker/jtreg/nullness/defaultsPersist25/ReferenceInfoUtil.java
+++ checker/jtreg/nullness/defaultsPersist25/ReferenceInfoUtil.java
@@ -1,6 +1,10 @@
-import java.lang.classfile.*;
-import java.lang.classfile.attribute.*;
-import java.util.*;
+import java.lang.classfile.Attributes;
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.MethodModel;
+import java.lang.classfile.attribute.AttributedElement;
+import java.lang.classfile.attribute.TypeAnnotation;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReferenceInfoUtil {
 
