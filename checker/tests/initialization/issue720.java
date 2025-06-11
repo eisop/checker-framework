015e31fc5aa2f1a3a6c07a71460c639565a95b1f
Billy Bai <y5bai@uwaterloo.ca>
2025-06-11 00:10:31 -0400

    refine the test case to be clearer.

--- checker/tests/initialization/issue720.java
+++ checker/tests/initialization/issue720.java
@@ -4,42 +4,28 @@
 import org.checkerframework.checker.initialization.qual.NotOnlyInitialized;
 import org.checkerframework.checker.initialization.qual.UnderInitialization;
 import org.checkerframework.checker.initialization.qual.UnknownInitialization;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 class Issue720 {
-    @NotOnlyInitialized @NonNull Issue720Field f = new Issue720Field();
+    @NotOnlyInitialized Object f = new Object();
 
-    Issue720() {
-        foo1();
-        foo2();
-    }
-
-    void foo1(@UnderInitialization Issue720 this) {
+    void fieldAccess1(@UnderInitialization Issue720 this) {
         // @NotOnlyInitialized should be correctly adapted to @UnknownInitialization
         // by @UnderInitialization.
-        // :: error: (dereference.of.nullable)
-        f.FieldAccess1();
+        // :: error: (dereference.of.nullable) :: error: (method.invocation.invalid)
+        f.hashCode();
     }
 
-    void foo2(@UnknownInitialization Issue720 this) {
+    void fieldAccess2(@UnknownInitialization Issue720 this) {
         // @NotOnlyInitialized should be correctly adapted to @UnknownInitialization
         // by @UnknownInitialization.
-        // :: error: (dereference.of.nullable)
-        f.FieldAccess1();
+        // :: error: (dereference.of.nullable) :: error: (method.invocation.invalid)
+        f.hashCode();
     }
 
-    void foo3() {
+    void fieldAccess3() {
         // @NotOnlyInitialized should be correctly adapted to @Initialized by @Initialized.
         // This is the only way to enter then branch in the issue. The correct adaption ensures the
         // correct use of @NotOnlyInitialized.
-        f.FieldAccess2();
+        f.hashCode();
     }
-}
-
-class Issue720Field {
-    @NotOnlyInitialized Object o = new Object();
-
-    void FieldAccess1(@UnknownInitialization Issue720Field this) {}
-
-    void FieldAccess2() {}
 }
