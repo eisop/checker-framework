caa166ac38a0e7c731903487d147206060739761
Werner Dietl <wdietl@gmail.com>
2025-06-26 18:33:03 -0400

    Add javadoc

--- framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
+++ framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
@@ -1563,6 +1563,7 @@ public class ValueTransfer extends CFTransfer {
         AND;
     }
 
+    /** A list containing the two possible Boolean values. */
     @SuppressWarnings("BooleanLiteral") // TODO: is there a performance difference?
     private static final List<Boolean> ALL_BOOLEANS =
             Arrays.asList(new Boolean[] {Boolean.TRUE, Boolean.FALSE});
