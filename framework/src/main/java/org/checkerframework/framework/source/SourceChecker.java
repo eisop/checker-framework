8131a526c462065aef78d59bf5f34f73af7314be
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-07-09 20:40:28 -0400

    Refactor cache to determine if an element is annotatedfor

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -2995,7 +2995,7 @@ public abstract class SourceChecker extends AbstractTypeProcessor implements Opt
      * @param elt the source code element to check, or null
      * @return true if the element is annotated for this checker or an upstream checker
      */
-    private boolean isAnnotatedForThisCheckerOrUpstreamChecker(@Nullable Element elt) {
+    protected boolean isAnnotatedForThisCheckerOrUpstreamChecker(@Nullable Element elt) {
         if (elt == null || !useConservativeDefaultsSource) {
             return false;
         }
