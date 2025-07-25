acf0e1a9dd877df0fc30e2333aa742fb8f7901a9
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-07-25 15:28:54 -0700

    Rename and more comments

--- checker/jtreg/nullness/onlyannotatedfor/AnnotatedForWithUse.java
+++ checker/jtreg/nullness/onlyannotatedfor/AnnotatedForWithUse.java
@@ -2,8 +2,9 @@
  * @test
  *
  * @summary Test that difference default applies to unannotated code.
- * @compile/fail/ref=OnlyAnnotatedForWithUse.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AonlyAnnotatedFor AnnotatedForWithUse.java
- * @compile/fail/ref=ConservativeDefaultWithUse.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AuseConservativeDefaultsForUncheckedCode=source AnnotatedForWithUse.java
+ * @compile/fail/ref=AnnotatedForWithUseNoFlag.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker AnnotatedForWithUse.java
+ * @compile/fail/ref=AnnotatedForWithUseOnlyAnnotatedFor.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AonlyAnnotatedFor AnnotatedForWithUse.java
+ * @compile/fail/ref=AnnotatedForWithUseConservativeDefault.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AuseConservativeDefaultsForUncheckedCode=source AnnotatedForWithUse.java
  */
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.AnnotatedFor;
@@ -18,9 +19,13 @@ public class AnnotatedForWithUse {
     @AnnotatedFor("nullness")
     class AnnotatedUse {
         void use(Unannotated u) {
+            // 1: OK, 2: OK, 3: Err
             @NonNull Object obj =  u.o;
+            // 1. Err, 2: Err, TODO want OK, 3:  OK, TODO want Err
             u.o = null;
+            // 1: OK, 2: OK, 3: Err
             u.get().toString();
+            // 1: Err, 2: Err TODO want OK, 3: Err
             u.set(null);
         }
     }
