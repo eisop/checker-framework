5a64e25a7bb3aa55fd6027bb15be32684d87ba5d
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-09-03 21:51:15 -0700

    Initial commit to introduce optimistic default

--- checker/jtreg/nullness/onlyannotatedfor/AnnotatedForWithUse.java
+++ checker/jtreg/nullness/onlyannotatedfor/AnnotatedForWithUse.java
@@ -5,6 +5,8 @@
  * @compile/fail/ref=AnnotatedForWithUseNoFlag.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker AnnotatedForWithUse.java
  * @compile/fail/ref=AnnotatedForWithUseOnlyAnnotatedFor.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AonlyAnnotatedFor AnnotatedForWithUse.java
  * @compile/fail/ref=AnnotatedForWithUseConservativeDefault.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AuseConservativeDefaultsForUncheckedCode=source AnnotatedForWithUse.java
+ * @compile/fail/ref=AnnotatedForWithUseOptimisticDefault.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AuseOptimisticDefaultsForUncheckedCode=source AnnotatedForWithUse.java
+ * @compile/fail/ref=AnnotatedForWithUseOptimisticDefaultOnlyAnnotatedFor.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AuseOptimisticDefaultsForUncheckedCode=source -AonlyAnnotatedFor AnnotatedForWithUse.java
  */
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.AnnotatedFor;
@@ -19,13 +21,13 @@ public class AnnotatedForWithUse {
     @AnnotatedFor("nullness")
     class AnnotatedUse {
         void use(Unannotated u) {
-            // 1: OK, 2: OK, 3: Err
+            // 1: OK, 2: OK, 3: Err. 4: OK
             @NonNull Object obj =  u.o;
-            // 1. Err, 2: Err, TODO want OK, 3:  OK, TODO want Err
+            // 1. Err, 2: Err, TODO want OK, 3:  OK, TODO want Err, 4: Err, TODO want OK
             u.o = null;
-            // 1: OK, 2: OK, 3: Err
+            // 1: OK, 2: OK, 3: Err, 4: OK
             u.get().toString();
-            // 1: Err, 2: Err TODO want OK, 3: Err
+            // 1: Err, 2: Err TODO want OK, 3: Err, 4: OK
             u.set(null);
         }
     }
