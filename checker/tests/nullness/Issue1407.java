679e062b34aeee206f529bd1940766175368244e
Billy Bai <y5bai@uwaterloo.ca>
2025-09-25 00:01:48 -0400

    Add a test case for issue1407

---
+++ checker/tests/nullness/Issue1407.java
@@ -0,0 +1,21 @@
+// Test case for Issue #1407
+// https://github.com/eisop/checker-framework/issues/1407
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class Issue1407 {
+    void inlinedCondition(@Nullable Object obj) {
+        if(obj != null){
+            obj.toString();
+        }
+    }
+
+    void wrappedCondition(@Nullable Object obj) {
+        boolean cond = obj != null;
+        if(cond){
+            // :: error: (dereference.of.nullable)
+            obj.toString();
+        }
+    }
+}
