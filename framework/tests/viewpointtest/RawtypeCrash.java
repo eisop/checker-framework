669c9b3b03b0c69c550476994f80877b497b1596
Werner Dietl <wdietl@gmail.com>
2026-06-14 12:33:13 -0400

    TODO: remove test once all-systems is used.

--- framework/tests/viewpointtest/RawtypeCrash.java
+++ framework/tests/viewpointtest/RawtypeCrash.java
@@ -1,3 +1,4 @@
+// TODO: remove once this checker runs `all-systems` tests.
 public class RawtypeCrash<V> {
     RawtypeCrash<? extends V> nullObj = null;
     Object obj = ((RawtypeCrash) null).nullObj;
