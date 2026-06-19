f685a5def44de6f6e074dcf26e2a694dc59bca39
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2026-06-19 14:13:50 -0400

    Clarify TYPE default qualifier behavior

--- framework/tests/viewpointtest/DefaultQualifierTest.java
+++ framework/tests/viewpointtest/DefaultQualifierTest.java
@@ -7,7 +7,47 @@ import viewpointtest.quals.*;
         value = A.class,
         locations = {TypeUseLocation.TYPE})
 public class DefaultQualifierTest {
-    // :: error: (type.invalid.annotations.on.use) :: error: (super.invocation.invalid) :: warning:
-    // (inconsistent.constructor.type)
+    // :: error: (type.invalid.annotations.on.use)
+    // :: error: (super.invocation.invalid)
+    // :: warning: (inconsistent.constructor.type)
     @B DefaultQualifierTest() {}
+}
+
+/**
+ * Explicit {@link A} on the class matches the TYPE default; {@link B} on the constructor is
+ * invalid.
+ */
+@DefaultQualifier(
+        value = A.class,
+        locations = {TypeUseLocation.TYPE})
+@A class DefaultQualifierExplicitAOnClass {
+    // :: error: (type.invalid.annotations.on.use)
+    // :: error: (super.invocation.invalid)
+    // :: warning: (inconsistent.constructor.type)
+    @B DefaultQualifierExplicitAOnClass() {}
+}
+
+/** Explicit {@link B} on the class overrides the TYPE default of {@link A}. */
+@DefaultQualifier(
+        value = A.class,
+        locations = {TypeUseLocation.TYPE})
+@B @SuppressWarnings("inconsistent.constructor.type")
+class DefaultQualifierExplicitBOnClass {
+    // :: error: (super.invocation.invalid)
+    DefaultQualifierExplicitBOnClass() {}
+}
+
+/** The TYPE default also applies to nested class declarations. */
+@DefaultQualifier(
+        value = A.class,
+        locations = {TypeUseLocation.TYPE})
+// :: error: (super.invocation.invalid)
+// :: warning: (inconsistent.constructor.type)
+@A class DefaultQualifierNestedClasses {
+    class DefaultedNested {
+        // :: error: (type.invalid.annotations.on.use)
+        // :: error: (super.invocation.invalid)
+        // :: warning: (inconsistent.constructor.type)
+        @B DefaultedNested() {}
+    }
 }
