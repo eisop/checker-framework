c0904b55219e9b95193cb60ea158e50def553f97
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2026-07-28 19:20:47 -0400

    Test viewpoint adaptation of F-bounded types

--- framework/tests/viewpointtest/SelfReferentialType.java
+++ framework/tests/viewpointtest/SelfReferentialType.java
@@ -4,17 +4,27 @@ import viewpointtest.quals.*;
 
 public class SelfReferentialType<C extends SelfReferentialType<C>> {
     // :: error: (super.invocation.invalid) :: warning: (inconsistent.constructor.type)
-    public @A SelfReferentialType() {}
+    public @ReceiverDependentQual SelfReferentialType() {}
 
     void createInstances() {
-        SelfReferentialType rawtypeInstance = new @A SelfReferentialType();
-        SelfReferentialType<C> genericInstance = new @A SelfReferentialType<>();
-        SelfReferentialType<?> wildcardInstance = new @A SelfReferentialType<C>();
+        @A SelfReferentialType rawtypeInstance = new @A SelfReferentialType();
+        @A SelfReferentialType<C> aGenericInstance = new @A SelfReferentialType<>();
+        @B SelfReferentialType<C> bGenericInstance = new @B SelfReferentialType<>();
+        @A SelfReferentialType<?> wildcardInstance = new @A SelfReferentialType<C>();
+        // :: error: (assignment.type.incompatible)
+        @B SelfReferentialType<C> incorrectlyAdaptedToB = new @A SelfReferentialType<>();
+        // :: error: (assignment.type.incompatible)
+        @A SelfReferentialType<C> incorrectlyAdaptedToA = new @B SelfReferentialType<>();
     }
 
     <D extends SelfReferentialType<D>> void createInstancesUsingMethodTypeParameter() {
-        SelfReferentialType rawtypeInstance = new @A SelfReferentialType();
-        SelfReferentialType<D> genericInstance = new @A SelfReferentialType<>();
-        SelfReferentialType<?> wildcardInstance = new @A SelfReferentialType<D>();
+        @A SelfReferentialType rawtypeInstance = new @A SelfReferentialType();
+        @A SelfReferentialType<D> aGenericInstance = new @A SelfReferentialType<>();
+        @B SelfReferentialType<D> bGenericInstance = new @B SelfReferentialType<>();
+        @A SelfReferentialType<?> wildcardInstance = new @A SelfReferentialType<D>();
+        // :: error: (assignment.type.incompatible)
+        @B SelfReferentialType<D> incorrectlyAdaptedToB = new @A SelfReferentialType<>();
+        // :: error: (assignment.type.incompatible)
+        @A SelfReferentialType<D> incorrectlyAdaptedToA = new @B SelfReferentialType<>();
     }
 }
