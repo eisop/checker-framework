fb319b7a4301e0b841f42ab177feeddf574f2b37
Werner Dietl <wdietl@gmail.com>
2026-08-07 11:23:17 -0400

    Fix checkstyle: add missing javadocs to TestDiagnosticUtilsTest

--- framework-test/src/test/java/org/checkerframework/framework/test/diagnostics/TestDiagnosticUtilsTest.java
+++ framework-test/src/test/java/org/checkerframework/framework/test/diagnostics/TestDiagnosticUtilsTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+/** Tests for {@link TestDiagnosticUtils}. */
 public class TestDiagnosticUtilsTest {
 
+    /** Tests parsing a diagnostic string with an empty position. */
     @Test
     public void testEmptyDiagnosticPosition() {
         String diagnosticString = "messageKey $$ 0 $$  $$ readableMessage";
