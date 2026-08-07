37a53d220c95ad750f982dd0ad476cfc762342ef
Werner Dietl <wdietl@gmail.com>
2026-08-07 10:43:18 -0400

    Fix CI: use TaintingChecker for jtreg test

--- checker/jtreg/stubs/tvbounds/Use.java
+++ checker/jtreg/stubs/tvbounds/Use.java
@@ -2,10 +2,10 @@
  * @test
  * @summary Test that type-parameter upper-bound annotations reach the ATV
  *
- * @compile/fail/ref=Use.out -XDrawDiagnostics -processor org.checkerframework.framework.testchecker.typedeclbounds.TypeDeclBoundsChecker -Astubs=typeparambound.astub Use.java
+ * @compile/fail/ref=Use.out -XDrawDiagnostics -processor org.checkerframework.checker.tainting.TaintingChecker -Astubs=typeparambound.astub Use.java
  */
 
-import org.checkerframework.framework.testchecker.typedeclbounds.quals.Bottom;
+import org.checkerframework.checker.tainting.qual.Untainted;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -13,13 +13,13 @@ import java.util.Iterator;
 public class Use {
     void argAboveBound(Iterator<String> it) {}
 
-    void argAtBound(Iterator<@Bottom String> it) {}
+    void argAtBound(Iterator<@Untainted String> it) {}
 
     void methodArgAboveBound() {
         Collections.<String>emptyList();
     }
 
     void methodArgAtBound() {
-        Collections.<@Bottom String>emptyList();
+        Collections.<@Untainted String>emptyList();
     }
 }
