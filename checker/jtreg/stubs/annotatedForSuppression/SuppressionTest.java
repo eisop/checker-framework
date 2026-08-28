6a96de06bcdcc8b8a2b2963ea97fb25b9c078008
Werner Dietl <wdietl@gmail.com>
2026-08-28 11:29:15 -0400

    Update SuppressionTest to pass -AmergeStubsWithSource

--- checker/jtreg/stubs/annotatedForSuppression/SuppressionTest.java
+++ checker/jtreg/stubs/annotatedForSuppression/SuppressionTest.java
@@ -4,10 +4,10 @@
  * AnnotatedFor scope, so -AonlyAnnotatedFor no longer suppresses warnings in it.
  *
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AonlyAnnotatedFor SuppressionTest.java
- * @compile/fail/ref=WithStub.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AonlyAnnotatedFor -Astubs=SuppressionTest.astub SuppressionTest.java
+ * @compile/fail/ref=WithStub.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AonlyAnnotatedFor -AmergeStubsWithSource -Astubs=SuppressionTest.astub SuppressionTest.java
  *
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AuseConservativeDefaultsForUncheckedCode=source SuppressionTest.java
- * @compile/fail/ref=WithStubConservativeSource.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AuseConservativeDefaultsForUncheckedCode=source -Astubs=SuppressionTest.astub SuppressionTest.java
+ * @compile/fail/ref=WithStubConservativeSource.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext -AuseConservativeDefaultsForUncheckedCode=source -AmergeStubsWithSource -Astubs=SuppressionTest.astub SuppressionTest.java
  */
 
 package annotatedforsuppression;
