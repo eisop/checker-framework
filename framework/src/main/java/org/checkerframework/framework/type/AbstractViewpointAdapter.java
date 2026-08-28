d91aed95f824f8661ac39c7c171ab1083aed9586
Werner Dietl <wdietl@gmail.com>
2026-08-28 04:28:01 -0400

    Code style and grammar improvements

--- framework/src/main/java/org/checkerframework/framework/type/AbstractViewpointAdapter.java
+++ framework/src/main/java/org/checkerframework/framework/type/AbstractViewpointAdapter.java
@@ -148,7 +148,7 @@ public abstract class AbstractViewpointAdapter implements ViewpointAdapter {
         constructorType.setParameterTypes(unsubstitutedConstructorType.getParameterTypes());
         constructorType.setTypeVariables(unsubstitutedConstructorType.getTypeVariables());
         constructorType.setReturnType(unsubstitutedConstructorType.getReturnType());
-        // Recompute the vararg type to ensure it points to the newly updated parameter list.
+        // Recompute the vararg type to ensure it corresponds to the newly updated parameter list.
         constructorType.computeVarargType();
     }
 
@@ -216,7 +216,7 @@ public abstract class AbstractViewpointAdapter implements ViewpointAdapter {
         methodType.setReceiverType(unsubstitutedMethodType.getReceiverType());
         methodType.setParameterTypes(unsubstitutedMethodType.getParameterTypes());
         methodType.setTypeVariables(unsubstitutedMethodType.getTypeVariables());
-        // Recompute the vararg type to ensure it points to the newly updated parameter list.
+        // Recompute the vararg type to ensure it corresponds to the newly updated parameter list.
         methodType.computeVarargType();
     }
 
